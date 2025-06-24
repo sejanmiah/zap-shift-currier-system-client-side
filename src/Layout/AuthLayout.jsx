7fe3b2113d4aecfb75ace0069effc0fb27ea13c3
sejanmiah <sejankhan931@gmail.com>
2025-06-25 01:00:48 +0400

    Login Page desine and react hook form install for dynamic

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import AuthImage from '../assets/authImage.png'
-import ProFastLogo from '../Pages/Shared/ProFastLogo/ProFastLogo';
+import React from "react";
+import { Outlet } from "react-router";
+import AuthImage from "../assets/authImage.png";
+import ProFastLogo from "../Pages/Shared/ProFastLogo/ProFastLogo";
 
 const AuthLayout = () => {
-    return (
+  return (
     <div className="bg-base-200 p-10 min-h-screen">
-        <div>
-            <ProFastLogo></ProFastLogo>
-        </div>
+      <div>
+        <ProFastLogo></ProFastLogo>
+      </div>
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img
-          src={AuthImage}
-          className="max-w-sm rounded-lg"
-        />
-        <div>
+                    
+          <div className="flex-1">
+            <img src={AuthImage} className="max-w-sm rounded-lg" />
+          </div>
+        <div className="flex-1">
           <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <Outlet></Outlet>
+          <Outlet></Outlet>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default AuthLayout;
--- src/Pages/Authentications/LogIn/LogIn.jsx
+++ src/Pages/Authentications/LogIn/LogIn.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const LogIn = () => {
+    const {register} = useForm()
   return (
     <div>
-        <h1>This is Login</h1>
+      <form>
+        <fieldset className="fieldset">
+
+          <label className="label">Email</label>
+          <input type="email" {...register("email")} className="input" placeholder="Email" />
+
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+
+          <button className="btn btn-neutral mt-4">Login</button>
+
+        </fieldset>
+      </form>
     </div>
   );
 };
